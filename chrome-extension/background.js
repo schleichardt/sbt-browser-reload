1472d3b9d017c90e1bd1a15b518d59c774c38c5a
Michael Schleichardt <michael@schleichardt.net>
2012-12-06 22:52:42 +0100

    chrome extension saves reload task across multiple reloads

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,39 +1,49 @@
 var tabs = [];
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  
- 
-  
 
-  if(pluginIsActive(tab)) {
-    deactivate(tab);
-  } else {
-    activate(tab);
-  } 
-  
+chrome.tabs.onUpdated.addListener(tab_updated);
+function tab_updated(tabId, changeInfo, tab) {
+    chrome.extension.getBackgroundPage().console.log("called5");
+    if(pluginIsActive(tab)) {
+        activate(tab);
+    } else {
+        deactivate(tab);
+    }
+};
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if(pluginIsActive(tab)) {
+        deactivate(tab);
+    } else {
+        activate(tab);
+    }
+
 });
 
 var pluginIsActive = function(tab) {
-  return tabs[tab.id] === true;
+    return tabs[tab.id] === true;
 }
 
 var activate = function(tab) {
-  tabs[tab.id] = true;
-  chrome.browserAction.setIcon({path:"icons/iconOn.png", tabId:tab.id});
-  
-  
-  var req = new XMLHttpRequest();
-  req.open("GET", "http://localhost:5555/poll", true);
-  req.onload = function(){
-      chrome.extension.getBackgroundPage().console.log("called3");
+    tabs[tab.id] = true;
+    chrome.browserAction.setIcon({path:"icons/iconOn.png", tabId:tab.id});
 
-      chrome.tabs.reload(tab.id, {bypassCache: true});
-      activate(tab);
-  };
-  req.send(null);
+
+    var req = new XMLHttpRequest();
+    req.open("GET", "http://localhost:5555/poll", true);
+    req.onload = function(){
+        chrome.extension.getBackgroundPage().console.log("called3");
+
+        if(pluginIsActive(tab)) {
+            chrome.tabs.reload(tab.id, {bypassCache: true});
+            activate(tab);
+        }
+    };
+    req.send(null);
 }
 
 var deactivate = function(tab) {
-  tabs[tab.id] = false;
-  chrome.browserAction.setIcon({path:"icons/iconOff.png", tabId:tab.id});
+    tabs[tab.id] = false;
+    chrome.browserAction.setIcon({path:"icons/iconOff.png", tabId:tab.id});
 }
